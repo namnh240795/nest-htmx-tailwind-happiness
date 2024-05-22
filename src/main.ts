cca41096a2b9d8a1612b5aecbdc062e7cc7cf848
Nam Nguyen <namnh240795@gmail.com>
2024-05-22 12:16:27 +0700

    chore: add back hbs due to the need of templating in ssr

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  hbs.registerPartials(join(__dirname, "..", "/views/partials"))
+  app.setViewEngine('hbs');
+
 
   await app.listen(3000);
 }
